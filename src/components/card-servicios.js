219e597fbe65dca06e80efc1a1272735213a4b93
Matias Belaus <matibelaus@gmail.com>
2021-07-16 23:50:01 -0300

    Fixing services responsive

--- src/components/card-servicios.js
+++ src/components/card-servicios.js
@@ -5,15 +5,17 @@ class CardServicio extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="card flip-card d-flex justify-content-center align-items-center">
-          <div className="flip-card-inner">
-            <div className="flip-card-front d-flex flex-column align-items-center justify-content-center">
-              <img src={this.props.img} className="img-service filter" alt="..." />
-              <h6 id="titulo">{this.props.title}</h6>
-            </div>
-            <div className="flip-card-back d-flex flex-column justify-content-center align-items-center">
-              <h6 className="mb-4">{this.props.title}</h6>
-              <p>{this.props.desc}</p>
+        <div className="col-lg-2 col-md-8 mx-5 my-2">
+          <div className="card flip-card d-flex justify-content-center align-items-center">
+            <div className="flip-card-inner">
+              <div className="flip-card-front d-flex flex-column align-items-center justify-content-center">
+                <img src={this.props.img} className="img-service filter" alt="..." />
+                <h6 id="titulo">{this.props.title}</h6>
+              </div>
+              <div className="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                <h6 className="mb-4">{this.props.title}</h6>
+                <p>{this.props.desc}</p>
+              </div>
             </div>
           </div>
         </div>
--- src/components/navbar-mobile.js
+++ src/components/navbar-mobile.js
@@ -21,35 +21,33 @@ class MobileNavbar extends React.Component {
 
   render() {
     return (
-      <div className="container-fluid" id="mobile-navbar">
-        <div className="row">
-          <div className="col-lg-12">
-            <div id="menu-bar">
-              <div id="menu" onClick={this.toggleClass} className={this.state.activeMenu ? 'change' : null}>
-                <div id="bar1" className="bar"></div>
-                <div id="bar2" className="bar"></div>
-                <div id="bar3" className="bar"></div>
-              </div>
-              <ul className={`nav ${this.state.activeNav ? 'change' : null}`} id="nav">
-                <li>
-                  <a href="/">Estudio Jurídico</a>
-                </li>
-                <li>
-                  <a href="/">Especialidades</a>
-                </li>
-                <li>
-                  <a href="/">Abogados</a>
-                </li>
-                <li>
-                  <a href="/">Blog</a>
-                </li>
-                <li>
-                  <a href="/">Contacto</a>
-                </li>
-              </ul>
+      <div className="row">
+        <div className="col-lg-12" id="mobile-navbar">
+          <div id="menu-bar">
+            <div id="menu" onClick={this.toggleClass} className={this.state.activeMenu ? 'change' : null}>
+              <div id="bar1" className="bar"></div>
+              <div id="bar2" className="bar"></div>
+              <div id="bar3" className="bar"></div>
             </div>
-            <div className={`menu-bg ${this.state.activeMenuBg ? 'change-bg' : null}`} id="menu-bg"></div>
+            <ul className={`nav ${this.state.activeNav ? 'change' : null}`} id="nav">
+              <li>
+                <a href="/">Estudio Jurídico</a>
+              </li>
+              <li>
+                <a href="/">Especialidades</a>
+              </li>
+              <li>
+                <a href="/">Abogados</a>
+              </li>
+              <li>
+                <a href="/">Blog</a>
+              </li>
+              <li>
+                <a href="/">Contacto</a>
+              </li>
+            </ul>
           </div>
+          <div className={`menu-bg ${this.state.activeMenuBg ? 'change-bg' : null}`} id="menu-bg"></div>
         </div>
       </div>
     );
