42deeba2e07879548757563704fbffd981084bdb
Matias Belaus <matibelaus@gmail.com>
2021-08-11 15:20:48 -0300

    Correcion de errores. Avance de seccion ESTUDIO

--- src/components/navbar-full.js
+++ src/components/navbar-full.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo_side from '../svg-services/logo_side.svg';
 import './nabvar-full.css';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 class FullNavbar extends React.Component {
   listener = null;
@@ -44,37 +44,51 @@ class FullNavbar extends React.Component {
           <div className="container">
             <div className="row align-items-center flex-wrap">
               <div className="col-xl-4 col-lg-12 d-xl-block d-lg-flex d-md-flex justify-content-center">
-                <Link to="/">
+                <NavLink exact to="/">
                   <img src={logo_side} alt="" className={this.state.logo ? 'logo-nav' : 'sticky-logo'} />
-                </Link>
+                </NavLink>
               </div>
               <div className="col-xl-8 col-lg-12 nav-full">
                 <ul className="nav-full">
-                  <Link to="/estudio-juridico">
-                    <li className="hvr-underline-from-center" id="estudio-page-link">
-                      Estudio Jurídico
+                  <Link to="estudio-juridico">
+                    <li className="hvr-underline-from-center principal" id="estudio-page-link">
+                      <NavLink activeClassName="current" to="/estudio-juridico">
+                        Estudio Jurídico
+                      </NavLink>
                     </li>
                   </Link>
+
                   <Link to="/abogados">
-                    <li className="hvr-underline-from-center">Abogados</li>
+                    <li className="hvr-underline-from-center principal">
+                      <NavLink activeClassName="current" to="/abogados">
+                        Abogados
+                      </NavLink>
+                    </li>
                   </Link>
 
-                  <li className="hvr-underline-from-center">Especialidades</li>
+                  <li className="hvr-underline-from-center principal">Especialidades</li>
 
                   <Link to="/contacto">
-                    <li className="hvr-underline-from-center">Contacto</li>
+                    <li className="hvr-underline-from-center principal">
+                      <NavLink activeClassName="current" to="/contacto">
+                        Contacto
+                      </NavLink>
+                    </li>
                   </Link>
 
-                  <li className="dropdown hvr-underline-from-center">
+                  <li className="dropdown hvr-underline-from-center principal">
                     <button className="nav-link dropdown-toggle button-toggle" href="#" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                      Otros
+                      <NavLink activeClassName="current" to="/blog">
+                        Otros
+                      </NavLink>
                     </button>
                     <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                       <Link to="/blog">
-                        <li className="dropdown-item">Blog</li>
+                        <li className="dropdown-item secundario">Blog</li>
                       </Link>
-                      <li className="dropdown-item">Prensa</li>
-                      <li className="dropdown-item">Novedades</li>
+
+                      <li className="dropdown-item secundario">Prensa</li>
+                      <li className="dropdown-item secundario">Novedades</li>
                     </ul>
                   </li>
                 </ul>
--- src/estudio-juridico.js
+++ src/estudio-juridico.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import './estudio-juridico.css';
 
 class EstudioJuridico extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <div className="container nav-breakpoints">
-          <h4>Estudio Juridico WORKS</h4>
+        <div className="container-fluid nav-breakpoints band-title">
+          <div className="row">
+            <div className="col-lg-6 col-md-7 top50">
+              <h2 className="estudio-title">¿POR QUÉ ELEGIRNOS?</h2>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 top50">
+              <p className="estudio-p">Contamos con una amplia trayectoria en el rubro, siempre trabajando de manera ética, honesta y sincera, procurando mantener relaciones a largo plazo con nuestros clientes. Somos recomendados permanentemente, lo que nos demuestra que vamos por el camino correcto.</p>
+              <p className="estudio-p">Trabajamos y nos esforzamos por nuestros clientes, entendiendo su problema y generando todas las alternativas para solucionarlo.</p>
+              <p className="estudio-p">Como consultores hemos generado resultados comprobables en las empresas donde hemos intervenido.</p>
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
